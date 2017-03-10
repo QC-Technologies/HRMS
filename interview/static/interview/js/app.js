d442cdab83b424fd1223485c636f14ad9af86fcb
Afnan Nazir <afnannazir.qc@gmail.com>
2017-03-10 15:39:15 +0500

    Angular material ui added

--- interview/static/interview/js/app.js
+++ interview/static/interview/js/app.js
@@ -2,7 +2,7 @@
  * Created by afnan on 8/11/16.
  */
 define(['require', 'angular-route'], function(require){
-    var app =  angular.module('interview', ['ngRoute', 'ui.bootstrap']);
+    var app =  angular.module('interview', ['ngRoute', 'ui.bootstrap', 'ngMaterial']);
     app.init = function(){
         angular.bootstrap(document, ['interview']);
     };
